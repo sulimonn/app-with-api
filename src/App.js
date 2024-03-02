e3e9d47d6948763ff6c0c73a7ab8840d04343d9d
sulimonn <gsuli836@gmail.com>
2024-03-02 12:45:59 +0600

    app padding bottom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import UsersList from './components/UsersList';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 w-screen">
+    <div className="min-h-screen bg-gray-900 w-screen pb-4">
       <UsersList />
     </div>
   );
